1c8f318d6810b888294346426ffd04276c3dafdf
dknelson9876 <dknelson9876@gmail.com>
2023-10-17 12:20:02 -0600

    Update zHal to handle other blue pills

--- Inc/zHal.h
+++ Inc/zHal.h
@@ -42,6 +42,23 @@ extern "C" {
 
 #endif 
 
+// BluePill
+#ifdef STM32F103xB
+#include "stm32f1xx_hal.h"
+
+#define _ZHAL_UART_SET_TXI(uartHandle) __ZHAL_SET(uartHandle->CR1, (1 << 7))
+#define _ZHAL_UART_RESET_TXI(uartHandle) __ZHAL_RESET(uartHandle->CR1, (1 << 7))
+#define _ZHAL_UART_SET_RXI(uartHandle) __ZHAL_SET(uartHandle->CR1, (1 << 5))
+#define _ZHAL_UART_RESET_RXI(uartHandle) __ZHAL_RESET(uartHandle->CR1,(1 << 5))
+
+#define _ZHAL_UART_RX_READY(uartHandle) (uartHandle->SR & (1 << 5))
+#define _ZHAL_UART_TX_READY(uartHandle) (uartHandle->SR & (1 << 7))
+
+#define _ZHAL_UART_TX_BYTE(uartHandle,byteVal) uartHandle->DR = byteVal
+#define _ZHAL_UART_RX_BYTE(uartHandle) ((uint8_t)uartHandle->DR)
+
+#endif 
+
 // BlackPill
 #ifdef STM32F401xC
 #include "stm32f4xx_hal.h"
