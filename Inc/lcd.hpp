cfce03599e6b8b6fdc1da212503caadf551fb70d
izeek182 <izeek182@gmail.com>
2023-10-23 23:42:16 -0600

    added image drawing and more

--- Inc/lcd.hpp
+++ Inc/lcd.hpp
@@ -2,6 +2,7 @@
 #define __LCD_CONTROL
 
 #include "zHal.h"
+#include "imageData.h"
 
 #define LCD_W 320
 #define LCD_H 480
@@ -46,6 +47,44 @@ typedef struct
     uint8_t G;
     uint8_t B;
 } Color;
+
+class LCD_Spi
+{
+    SPI_HandleTypeDef *Spi;
+
+    GPIO_TypeDef *DataComSel_GPIO;
+    uint16_t DataComSel_pin;
+
+    GPIO_TypeDef *chipSel_GPIO;
+    uint16_t chipSel_pin;
+
+    GPIO_TypeDef *LcdRst_GPIO;
+    uint16_t LcdRst_pin;
+
+    GPIO_TypeDef *LcdBackLed_GPIO;
+    uint16_t LcdBackLed_pin;
+
+    LCD_Spi(
+        SPI_HandleTypeDef *Spi,
+        GPIO_TypeDef *DataComSel_GPIO,
+        uint16_t DataComSel_pin,
+        GPIO_TypeDef *chipSel_GPIO,
+        uint16_t chipSel_pin,
+        GPIO_TypeDef *LcdRst_GPIO,
+        uint16_t LcdRst_pin,
+        GPIO_TypeDef *LcdBackLed_GPIO,
+        uint16_t LcdBackLed_pin) : Spi(Spi),
+                                   DataComSel_GPIO(DataComSel_GPIO),
+                                   DataComSel_pin(DataComSel_pin),
+                                   chipSel_GPIO(chipSel_GPIO),
+                                   chipSel_pin(chipSel_pin),
+                                   LcdRst_GPIO(LcdRst_GPIO),
+                                   LcdRst_pin(LcdRst_pin),
+                                   LcdBackLed_GPIO(LcdBackLed_GPIO),
+                                   LcdBackLed_pin(LcdBackLed_pin)
+    {
+    }
+};
 class LCD_Controller
 {
 private:
@@ -70,7 +109,7 @@ private:
     {
         HAL_GPIO_WritePin(chipSel_GPIO, chipSel_pin, GPIO_PIN_RESET);
         HAL_GPIO_WritePin(DataComSel_GPIO, DataComSel_pin, GPIO_PIN_RESET); // Signals that it is a command being written
-        HAL_SPI_Transmit(Spi, &cmd, 1, 0);
+        HAL_SPI_Transmit(Spi, &cmd, 1, HAL_MAX_DELAY);
         HAL_GPIO_WritePin(chipSel_GPIO, chipSel_pin, GPIO_PIN_SET);
     }
 
@@ -78,19 +117,36 @@ private:
     {
         HAL_GPIO_WritePin(chipSel_GPIO, chipSel_pin, GPIO_PIN_RESET);
         HAL_GPIO_WritePin(DataComSel_GPIO, DataComSel_pin, GPIO_PIN_SET); // Signals that data being written
-        HAL_SPI_Transmit(Spi, &data, 1, 0);
+        HAL_SPI_Transmit(Spi, &data, 1, HAL_MAX_DELAY);
         HAL_GPIO_WritePin(chipSel_GPIO, chipSel_pin, GPIO_PIN_SET);
     }
 
-    void writeDataToLcd(uint32_t dataLen, uint8_t *dataLSB)
+    void writeDataToLcd(uint32_t dataLen, uint8_t *data)
     {
         HAL_GPIO_WritePin(chipSel_GPIO, chipSel_pin, GPIO_PIN_RESET);
         HAL_GPIO_WritePin(DataComSel_GPIO, DataComSel_pin, GPIO_PIN_SET); // Signals that data being written
-        HAL_SPI_Transmit(Spi, dataLSB, dataLen, 0);
+        // uint16_t status=  
+        HAL_SPI_Transmit(Spi, data, dataLen, HAL_MAX_DELAY);
+        // uint8_t volatile testing = 10;
+        // if(status){
+        //     testing = 10+1;
+        // }
         // HAL_SPI_Transmit(Spi, &dataLSB, 1, 0);
         HAL_GPIO_WritePin(chipSel_GPIO, chipSel_pin, GPIO_PIN_SET);
     }
-
+    void writeLUTDataToLcd(uint32_t dataLen, uint8_t *data, uint8_t *LUT){
+        HAL_GPIO_WritePin(chipSel_GPIO, chipSel_pin, GPIO_PIN_RESET);
+        HAL_GPIO_WritePin(DataComSel_GPIO, DataComSel_pin, GPIO_PIN_SET); // Signals that data being written
+        for (uint32_t i = 0; i < dataLen; i++)
+        {
+            uint8_t*  c1 = &(LUT[((data[i]>>0)&0xF)*4]);
+            uint8_t*  c2 = &(LUT[((data[i]>>4)&0xF)*4]);
+            HAL_SPI_Transmit(Spi,c2, 3, HAL_MAX_DELAY);
+            HAL_SPI_Transmit(Spi,c1, 3, HAL_MAX_DELAY);
+        }
+        
+        HAL_GPIO_WritePin(chipSel_GPIO, chipSel_pin, GPIO_PIN_SET);
+    }
     void writeColorDataToLcd(uint32_t dataLen, Color color)
     {
         HAL_GPIO_WritePin(chipSel_GPIO, chipSel_pin, GPIO_PIN_RESET);
@@ -101,12 +157,6 @@ private:
         for (uint32_t i = 0; i < dataLen; i++)
         {
             HAL_SPI_Transmit(Spi, data, 3, HAL_MAX_DELAY);
-            // HAL_SPI_Transmit(Spi, &G, 1, HAL_MAX_DELAY);
-            // HAL_SPI_Transmit(Spi, &B, 1, HAL_MAX_DELAY);
-            // HAL_SPI_Transmit(Spi, &MSB, 1, HAL_MAX_DELAY);
-            // HAL_SPI_Transmit(Spi, &MSB, 1, HAL_MAX_DELAY);
-            // HAL_SPI_Transmit(Spi, &MSB, 1, HAL_MAX_DELAY);
-            // HAL_SPI_Transmit(Spi, &LSB, 1, HAL_MAX_DELAY);
         }
         HAL_GPIO_WritePin(chipSel_GPIO, chipSel_pin, GPIO_PIN_SET);
     }
@@ -139,20 +189,10 @@ private:
         uint16_t xEnd = x + w;
         uint8_t xdata[4] = {(uint8_t)(x >> 8), (uint8_t)(0xFF & x), (uint8_t)(xEnd >> 8), (uint8_t)(xEnd & 0xff)};
         writeCmdToLcd(COL_RANGE_SET, 4, (uint8_t *)&xdata);
-        // LCD_WR_REG(ROW_RANGE_SET);
-        // LCD_WR_DATA(x >> 8);
-        // LCD_WR_DATA(0x00FF & x);
-        // LCD_WR_DATA(xEnd >> 8);
-        // LCD_WR_DATA(0x00FF & xEnd);
 
         uint16_t yEnd = y + h;
         uint8_t ydata[4] = {(uint8_t)(y >> 8), (uint8_t)(0xFF & y), (uint8_t)(yEnd >> 8), (uint8_t)(yEnd & 0xff)};
         writeCmdToLcd(ROW_RANGE_SET, 4, (uint8_t *)&ydata);
-        // LCD_WR_REG(COL_RANGE_SET);
-        // LCD_WR_DATA(yStar >> 8);
-        // LCD_WR_DATA(0x00FF & yStar);
-        // LCD_WR_DATA(yEnd >> 8);
-        // LCD_WR_DATA(0x00FF & yEnd);
     }
 
     void WriteMem(uint16_t len, uint8_t *data)
@@ -182,101 +222,12 @@ private:
         HAL_Delay(120);
 
         writeCmdToLcd(DIS_ON);
-        // HAL_Delay(5);
         HAL_Delay(120);
-        // writeCmdToLcd(0xCF);  // Power Control B
-        // writeDataToLcd(0x00); //
-        // writeDataToLcd(0xC9); // C1
-        // writeDataToLcd(0X30);
-        // writeCmdToLcd(0xED); // Power On sequence control
-        // writeDataToLcd(0x64);
-        // writeDataToLcd(0x03);
-        // writeDataToLcd(0X12);
-        // writeDataToLcd(0X81);
-        // writeCmdToLcd(0xE8); // Driver timing control A
-        // writeDataToLcd(0x85);
-        // writeDataToLcd(0x10);
-        // writeDataToLcd(0x7A);
-        // writeCmdToLcd(0xCB);  // Power Control A
-        // writeDataToLcd(0x39); // Default
-        // writeDataToLcd(0x2C); // Default
-        // writeDataToLcd(0x00); // Default
-        // writeDataToLcd(0x34); // Default
-        // writeDataToLcd(0x02); // Default
-        // writeCmdToLcd(0xF7);  // Pump ratio control
-        // writeDataToLcd(0x20);
-        // writeCmdToLcd(0xEA); // Driver timing control B
-        // writeDataToLcd(0x00);
-        // writeDataToLcd(0x00);
-        // writeCmdToLcd(0xC0);  // Power control
-        // writeDataToLcd(0x1B); // VRH[5:0]  -> 4.2V ?
-        // writeCmdToLcd(0xC1);  // Power control
-        // writeDataToLcd(0x00); // SAP[2:0];BT[3:0] 01
-        // writeCmdToLcd(0xC5);  // VCM control
-        // writeDataToLcd(0x30); // 3F
-        // writeDataToLcd(0x30); // 3C
-        // writeCmdToLcd(0xC7);  // VCM control2
-        // writeDataToLcd(0XB7);
-        // writeCmdToLcd(0x36); // Memory Access Control
-        // writeDataToLcd(0x08);
-        // writeCmdToLcd(0x3A);  // Pixel Format
-        // writeDataToLcd(0x55); // 16Bit Dpi 16bit Dbi
-        // writeCmdToLcd(0xB1);  // Frame Rate Control
-        // writeDataToLcd(0x00); // No Div
-        // writeDataToLcd(0x1A); // 73 Hz
-        // writeCmdToLcd(0xB6);  // Display Function Control
-        // writeDataToLcd(0x0A); // internal scan -- A_Gnd Default
-        // writeDataToLcd(0xA2);
-        // writeCmdToLcd(0xF2); // 3Gamma Function Disable
-        // writeDataToLcd(0x00);
-        // writeCmdToLcd(0x26); // Gamma curve selected
-        // writeDataToLcd(0x01);
-        // writeCmdToLcd(0xE0); // Set Gamma
-        // writeDataToLcd(0x0F);
-        // writeDataToLcd(0x2A);
-        // writeDataToLcd(0x28);
-        // writeDataToLcd(0x08);
-        // writeDataToLcd(0x0E);
-        // writeDataToLcd(0x08);
-        // writeDataToLcd(0x54);
-        // writeDataToLcd(0XA9);
-        // writeDataToLcd(0x43);
-        // writeDataToLcd(0x0A);
-        // writeDataToLcd(0x0F);
-        // writeDataToLcd(0x00);
-        // writeDataToLcd(0x00);
-        // writeDataToLcd(0x00);
-        // writeDataToLcd(0x00);
-        // writeCmdToLcd(0XE1); // Set Gamma
-        // writeDataToLcd(0x00);
-        // writeDataToLcd(0x15);
-        // writeDataToLcd(0x17);
-        // writeDataToLcd(0x07);
-        // writeDataToLcd(0x11);
-        // writeDataToLcd(0x06);
-        // writeDataToLcd(0x2B);
-        // writeDataToLcd(0x56);
-        // writeDataToLcd(0x3C);
-        // writeDataToLcd(0x05);
-        // writeDataToLcd(0x10);
-        // writeDataToLcd(0x0F);
-        // writeDataToLcd(0x3F);
-        // writeDataToLcd(0x3F);
-        // writeDataToLcd(0x0F);
-        // writeCmdToLcd(0x2B); // Set Page Address
-        // writeDataToLcd(0x00);
-        // writeDataToLcd(0x00);
-        // writeDataToLcd(0x01);
-        // writeDataToLcd(0x3f);
-        // writeCmdToLcd(0x2A); // Set Column Address
-        // writeDataToLcd(0x00);
-        // writeDataToLcd(0x00);
-        // writeDataToLcd(0x00);
-        // writeDataToLcd(0xef);
-        // writeCmdToLcd(0x11); // Exit Sleep
-        // HAL_Delay(120);
-        // writeCmdToLcd(0x29); // display on
     }
+
+
+
+public:
 
     void DisOn()
     {
@@ -288,7 +239,6 @@ private:
         writeCmdToLcd(DIS_OFF);
     }
 
-public:
     void LCD_Dir(LCD_Dir direction)
     {
         // xWinCmd = 0x2A;
@@ -367,10 +317,13 @@ public:
         writeCmdToLcd(DIS_INV_OFF);
     }
 
-    // void writeImageDate(uint16_t x, uint16_t y,uint16_t w, uint16_t h, uint8_t *frame)
-    // {
-    //     HAL_GPIO_WritePin(LcdBackLed_GPIO, LcdBackLed_pin, GPIO_PIN_RESET);
-    // }
+    void drawImage(uint16_t x, uint16_t y, ImageData image)
+    {
+        SetWindow(x, y, image.width-1, image.height-1);
+        writeCmdToLcd(MEM_SET);
+        writeLUTDataToLcd((image.width*image.height)/2, image.data,image.lut);
+        // WriteMemSingleCol(w * h, color);
+    }
 
     void drawRec(uint16_t x, uint16_t y, uint16_t w, uint16_t h, Color color)
     {
