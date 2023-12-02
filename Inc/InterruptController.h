c9e8cd31e277aa894f58fdcabd5bb87c9f09477b
izeek182 <izeek182@gmail.com>
2023-12-02 10:46:38 -0700

    Isaac's Super basic Interrupt Controller.

---
+++ Inc/InterruptController.h
@@ -0,0 +1,16 @@
+#ifndef INTERRUPTCONTROLLER_H
+#define INTERRUPTCONTROLLER_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+// Function declarations
+void InterruptController_enter();
+void InterruptController_leave();
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif // INTERRUPTCONTROLLER_H
